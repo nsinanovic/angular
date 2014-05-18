9ff8336fa5ed0602446f72b23c51585ce57c8ea8
dahmic2 <dahmic2@etf.unsa.ba>
2014-05-19 00:19:37 +0200

    dodatak

---
+++ db/migrate/20140518101229_create_advartisments.rb
@@ -0,0 +1,14 @@
+class CreateAdvartisments < ActiveRecord::Migration
+  def change
+    create_table :advartisments do |t|
+      t.string :title
+      t.text :description
+      t.date :published
+      t.date :expire
+      t.string :location
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
